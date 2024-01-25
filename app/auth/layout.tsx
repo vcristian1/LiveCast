23a3577384691cb1c4cc89336da1afdaf0107fd2
Vargas <cristian.a.vargas@accenture.com>
2024-01-25 13:18:37 -0600

    added new layout.tsx file within /Auth routes so the Auth Navbar is seen

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,9 +1,14 @@
-const AuthLayout = () => {
+const AuthLayout = ({
+    children
+}: {
+    children: React.ReactNode;
+}) => {
     return (
         <div>
-
+            <nav className="bg-red-500 p-1">AuthNavbar</nav>
+            {children}
         </div>
-    )
+    );
 }
 
 export default AuthLayout;
