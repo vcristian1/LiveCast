c60d0c4d23b7c48a492715bba90f760ec985fe74
Vargas <cristian.a.vargas@accenture.com>
2024-01-26 11:56:11 -0600

    removed line of code that seemed unneccessary from layout.tsx in app directory. Hoping this resolves deployment issues as locally runs fine

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,9 +12,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <ClerkProvider>
       <html lang="en">
