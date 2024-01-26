40837ab527b2f6da0ed013c31ca02fe41faa1fa6
Vargas <cristian.a.vargas@accenture.com>
2024-01-26 12:04:38 -0600

    env variable in vercel was mispelled causing the deployment issue

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { UserButton } from "@clerk/nextjs"
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-y-4">
+    <div className="flex flex-col gap-y-4 h-screen">
       <h1 className="text-white">Dashboard</h1>
       <UserButton afterSignOutUrl="/"/>
     </div>
