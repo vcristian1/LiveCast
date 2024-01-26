67ff54f3016254f8e5db3e45c3dbd6b41859586e
Vargas <cristian.a.vargas@accenture.com>
2024-01-26 13:27:47 -0600

    added appearance property in ClerkProvider to activate dark mode as the base theme, after downloaded npm package next/themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from '@clerk/themes';
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -16,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{baseTheme: dark}}>
       <html lang="en">
         <body className={inter.className}>
           {children}</body>
