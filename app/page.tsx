e022105a9f554d6f77a3c25ae77535252cf2f7d2
Vargas <cristian.a.vargas@accenture.com>
2024-01-26 11:32:36 -0600

    attempt to resolve build error

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,8 @@ import { UserButton } from "@clerk/nextjs"
 export default function Home() {
   return (
     <div className="flex flex-col gap-y-4">
-      <h1>Dashboard</h1>
+      <h1 className="text-white">Dashboard</h1>
       <UserButton afterSignOutUrl="/"/>
-    
     </div>
   )
 }
