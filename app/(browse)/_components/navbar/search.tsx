9bf6fe2c23da5fcb3fe8ac9cc010479babd2e22e
Vargas <cristian.a.vargas@accenture.com>
2024-01-31 12:00:46 -0600

    added styling to the searchicon and the button wrapping it

--- app/(browse)/_components/navbar/search.tsx
+++ app/(browse)/_components/navbar/search.tsx
@@ -12,8 +12,8 @@ export const Search = () => {
     return (
         <form className="relative w-full lg:w-[400px] flex items-center">
             <Input placeholder="Search"/>
-            <Button>
-                <SearchIcon />
+            <Button type="submit" size="sm" variant="secondary" className="rounded-l-none hover:opacity-75 transition duration-500">
+                <SearchIcon className="h-5 w-5 text-muted-foreground"/>
             </Button>
         </form>
     )
