7328255ced01ee89fb5e1d5f921515fe73d22a40
Vargas <cristian.a.vargas@accenture.com>
2024-01-31 12:43:47 -0600

    added logic to render the signin button when there is no user, also imported the button from shadcn and signinbutton from clerk

--- app/(browse)/_components/navbar/actions.tsx
+++ app/(browse)/_components/navbar/actions.tsx
@@ -1,10 +1,18 @@
-import { currentUser } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { SignInButton, currentUser } from "@clerk/nextjs";
 
 export const Actions = async () => {
     const user = await currentUser();
     return (
         <div className="flex items-center justify-end gap-x-2 ml-4 lg:ml-0">
             Actions!
+            {!user &&
+                <SignInButton>
+                    <Button>
+                        
+                    </Button>
+                </SignInButton>
+            }
         </div>
     )
 }
