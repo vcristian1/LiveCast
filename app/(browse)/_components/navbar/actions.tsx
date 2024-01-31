b881fca2567e6f2988a69fb0ec63e71adba4e4f8
Vargas <cristian.a.vargas@accenture.com>
2024-01-31 12:57:09 -0600

    in actions component, added logic for if there is a user signed in which would render the Dashboard button with a clapperboard icon from lucide react package

--- app/(browse)/_components/navbar/actions.tsx
+++ app/(browse)/_components/navbar/actions.tsx
@@ -1,18 +1,36 @@
 import { Button } from "@/components/ui/button";
-import { SignInButton, currentUser } from "@clerk/nextjs";
+import { SignInButton, SignOutButton, currentUser } from "@clerk/nextjs";
+import { Clapperboard } from "lucide-react";
+import Link from "next/link";
+
 
 export const Actions = async () => {
     const user = await currentUser();
     return (
         <div className="flex items-center justify-end gap-x-2 ml-4 lg:ml-0">
-            Actions!
-            {!user &&
+            {!user && (
                 <SignInButton>
                     <Button>
-                        
+                        Login
                     </Button>
                 </SignInButton>
-            }
+            )}
+            {!!user && (
+                <div className="flex items-center gap-x-4">
+                    <Button
+                    size="sm"
+                    variant="ghost"
+                    className="text-muted-foreground hover:text-primary transition duration-500"
+                    >
+                        <Link href={`/u/${user.username}`} className="flex space-x-1">
+                            <Clapperboard />
+                            <span>
+                                Dashboard
+                            </span>
+                        </Link>
+                    </Button>
+                </div>
+            )}
         </div>
     )
 }
