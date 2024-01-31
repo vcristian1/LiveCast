27b98bf8a7f3fa140598010d1fb0ee204c7cd0bc
Vargas <cristian.a.vargas@accenture.com>
2024-01-31 14:45:39 -0600

    added styling to logo.tsx so on small screens you can see the logo with no text

--- app/(browse)/_components/navbar/logo.tsx
+++ app/(browse)/_components/navbar/logo.tsx
@@ -12,11 +12,14 @@ const font = Poppins({
 export const Logo = () => {
     return (
         <Link href="/">
-            <div className='hidden lg:flex items-center gap-x-4 hover:opacity-75 transition duration-500'>
-                <div className='bg-white rounded-full p-1'>
+            <div className='flex items-center gap-x-4 hover:opacity-75 transition duration-500'>
+                <div className='bg-white rounded-full p-1 mr-12 shrink-0 lg:mr-0 lg:shrink'>
                     <Image width={32} height={32} alt="LiveCast" src="/spooky.svg"/>
                 </div>
-                <div className={cn(font.className)}>
+                <div className={cn(
+                    "hidden lg:block",
+                    font.className
+                    )}>
                     <p className='text-lg font-semibold'>
                         LiveCast
                     </p>
