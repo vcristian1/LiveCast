1c95bddc69babfbf00bcece4fc748dd7d5f90c8c
Vargas <cristian.a.vargas@accenture.com>
2024-01-31 15:06:30 -0600

    added styling to aside element within wrapper.tsx so the bg color is different, and the width is appropriate

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -6,7 +6,7 @@ export const Wrapper = ({
     children,
 }: WrapperProps) => {
     return (
-        <aside>
+        <aside className="fixed left-0 flex flex-col w-[80px] lg:w-60 h-full bg-background border-r border-[#2D2E35] Z-50">
             {children}
         </aside>
     )
