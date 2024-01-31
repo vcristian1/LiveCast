6e611663a154bc2687c1e406b8b850984aa41436
Vargas <cristian.a.vargas@accenture.com>
2024-01-31 12:08:06 -0600

    added styling to the search input so the focus visible is transparent as each time the user clicks you can see focus as if you tabbed to the search input

--- app/(browse)/_components/navbar/search.tsx
+++ app/(browse)/_components/navbar/search.tsx
@@ -11,7 +11,9 @@ export const Search = () => {
     console.log("I am logged here")
     return (
         <form className="relative w-full lg:w-[400px] flex items-center">
-            <Input placeholder="Search"/>
+            {/* I find the focus-visible: styling classes to be visually appealing to the eye, but they prevent the input from being fully accessible. 
+            Low vision users wouldn't be able to tell the search input is receiving any focus if it is transparent. -CV */}
+            <Input placeholder="Search" className="rounded-r-none focus-visible:ring-transparent focus-visible:ring-offset-0"/>
             <Button type="submit" size="sm" variant="secondary" className="rounded-l-none hover:opacity-75 transition duration-500">
                 <SearchIcon className="h-5 w-5 text-muted-foreground"/>
             </Button>
