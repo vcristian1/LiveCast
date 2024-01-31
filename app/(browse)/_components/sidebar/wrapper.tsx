f12cdcfa069bd08a4f48f85c15777455fe88fb1e
Vargas <cristian.a.vargas@accenture.com>
2024-01-31 15:20:24 -0600

    added the custom hook useSidebar in wrapper.tsx, added use client at the top since we are using state which means this is now a client component

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useSidebar } from "@/store/use-sidebar";
+
 interface WrapperProps {
     children: React.ReactNode;
 }
@@ -5,6 +9,7 @@ interface WrapperProps {
 export const Wrapper = ({
     children,
 }: WrapperProps) => {
+    const { collapsed } = useSidebar((state) => state);
     return (
         <aside className="fixed left-0 flex flex-col w-[80px] lg:w-60 h-full bg-background border-r border-[#2D2E35] Z-50">
             {children}
