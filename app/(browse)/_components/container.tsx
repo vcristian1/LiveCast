ecd3954a7fb121aac1895a31380d203ccb3b7708
Vargas <cristian.a.vargas@accenture.com>
2024-02-02 16:33:13 -0600

    downloaded usehooks-ts package so I can use the useMediaQuery hook for enabling the sidebar to automatically collapse if the width is less than 1024px

--- app/(browse)/_components/container.tsx
+++ app/(browse)/_components/container.tsx
@@ -1,4 +1,9 @@
 "use client";
+import { useEffect } from "react";
+import { useMediaQuery } from "usehooks-ts";
+
+import { cn } from "@/lib/utils";
+import { useSidebar } from "@/store/use-sidebar";
 
 interface ContainerProps {
     children: React.ReactNode,
@@ -7,8 +12,27 @@ interface ContainerProps {
 export const Container = ({
     children,
 }: ContainerProps) => {
+    const matches = useMediaQuery("(max-width: 1024px)");
+    const {
+        collapsed,
+        onCollapse,
+        onExpand
+    } = useSidebar((state) => state)
+
+    useEffect(() => {
+        if(matches){
+            onCollapse();
+        } else {
+            onExpand();
+        }
+    }, [matches, onCollapse, onExpand]);
+
+
     return (
-        <div>
+        <div className={cn(
+            "flex-1",
+            collapsed ? "ml-[70px]": "ml-[70px] lg:ml-60"
+        )}>
             {children}
         </div>
     )
