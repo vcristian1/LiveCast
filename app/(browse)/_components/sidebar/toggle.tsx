9cd3f92596ccbc45eca57fa43a6a44cd8f2e7186
Vargas <cristian.a.vargas@accenture.com>
2024-02-02 16:16:59 -0600

    downloaded tooltip from shadcn so we can have hints when the user hovers over the expand and collapse buttons, created hint.tsx in components directory, imported hint in toggle.tsx and wrapped the collapse and expand buttons with it

--- app/(browse)/_components/sidebar/toggle.tsx
+++ app/(browse)/_components/sidebar/toggle.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Hint } from "@/components/hint";
 import { Button } from "@/components/ui/button";
 import { useSidebar } from "@/store/use-sidebar";
 import { ArrowLeftFromLine, ArrowRightFromLine } from "lucide-react";
@@ -11,21 +12,27 @@ export const Toggle = () => {
         onCollapse,
     } = useSidebar((state) => state)
 
+    const label = collapsed ? "Expand" : "Collapse";
+
     return (
         <>
             {!collapsed && (
                 <div className="p-3 pl-6 mb-2 flex items-center w-full">
                     <p className="font-semibold text-primary">Suggested for you</p>
-                    <Button onClick={onCollapse} className="h-auto p-2 ml-auto transition duration-500" variant="ghost">
-                        <ArrowLeftFromLine className="h-4 w-4"/>
-                    </Button>
+                    <Hint label={label} side="right" asChild>
+                        <Button onClick={onCollapse} className="h-auto p-2 ml-auto transition duration-500" variant="ghost">
+                            <ArrowLeftFromLine className="h-4 w-4"/>
+                        </Button>
+                    </Hint>
                 </div>
             )}
             {collapsed && (
                 <div className="hidden lg:flex items-center w-full justify-center pt-4 mb-4">
-                    <Button onClick={onExpand} className="h-auto p-2 transition duration-500" variant="ghost">
-                        <ArrowRightFromLine className="h-4 w-4"/>
-                    </Button>
+                    <Hint label={label} side="right" asChild>
+                        <Button onClick={onExpand} className="h-auto p-2 transition duration-500" variant="ghost">
+                            <ArrowRightFromLine className="h-4 w-4"/>
+                        </Button>
+                    </Hint>
                 </div>
             )}
         </>
