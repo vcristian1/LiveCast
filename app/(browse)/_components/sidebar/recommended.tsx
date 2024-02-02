0439b924580670ad394e27e03a301e3688769670
Vargas <cristian.a.vargas@accenture.com>
2024-02-02 17:12:31 -0600

    created recommended.tsx within sidebar directory and rendered that within a div in the sidebar

---
+++ app/(browse)/_components/sidebar/recommended.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { User } from "@prisma/client"
+import { useSidebar } from "@/store/use-sidebar";
+
+interface RecommendedProps {
+    data: User[];
+}
+
+export const Recommended = ({
+    data, 
+}: RecommendedProps) => {
+    const { collapsed } = useSidebar((state) => state);
+
+    const showLabel = !collapsed && data.length > 0;
+
+    return (
+        <div>
+            {showLabel && (
+                <div className="pl-6 mb-4 text-muted-foreground">
+                    <p>For you</p>
+                </div>
+            )}
+        </div>
+    )
+}
