1a011f61e3c431b6ae2387d86175754477a4e1d2
Vargas <cristian.a.vargas@accenture.com>
2024-02-06 15:28:06 -0600

    removed nav container wrapping p tags and actions component in /[username]/page.tsx

--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -23,12 +23,10 @@ const UserPage = async ({
 
     return (
       <div className="flex flex-col gap-y-4 h-screen">
-        <nav className="p-1 items-center justify-between gap-y-8">
           <p>Username: {user.username}</p>
           <p>user ID: {user.id}</p>
           <p>is following: {`${isFollowing}`}</p>
           <Actions />
-        </nav>
       </div>
     )
 }
