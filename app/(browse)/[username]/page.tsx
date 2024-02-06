b58e090d6fc416028916c285532d4b1d79bc241f
Vargas <cristian.a.vargas@accenture.com>
2024-02-06 14:40:32 -0600

    after creating user-service.ts file, went into page,tsx of /[username] and made UserPage function an async function since we have params in user-service.ts

--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -1,12 +1,21 @@
+import { getUserByUsername } from "@/lib/user-service";
+import { notFound } from "next/navigation";
+
 interface UserPageProps {
   params: {
     username: string;
   }
 }
 
-const UserPage = ({
+const UserPage = async ({
   params,
 }: UserPageProps) => {
+    const user = await getUserByUsername(params.username)
+
+    if(!user) {
+      notFound();
+    }
+
     return (
       <div className="flex flex-col gap-y-4 h-screen">
         <nav className="p-1 items-center justify-between gap-y-8">
--- lib/user-service.ts
+++ lib/user-service.ts
@@ -1,11 +1,14 @@
 import { db } from "./db";
 
+// Define a function to fetch a user by their username
 export const getUserByUsername = async (username: string) => {
+    // Attempt to retrieve a user from the database by their username
     const user = await db.user.findUnique({
         where: {
             username,
         }
     });
 
+    // Return the retrieved user (or null if not found)
     return user;
 }
