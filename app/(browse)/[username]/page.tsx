f7b1f53a1dd39f824cd3d5ac72b8c9884e0d45a0
Vargas <cristian.a.vargas@accenture.com>
2024-02-06 14:45:41 -0600

    rendered additional user data in the UserPage in index.tsx of /[username]

--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -1,3 +1,4 @@
+import { isFollowingUser } from "@/lib/follow-service";
 import { getUserByUsername } from "@/lib/user-service";
 import { notFound } from "next/navigation";
 
@@ -16,10 +17,15 @@ const UserPage = async ({
       notFound();
     }
 
+    const isFollowing = await isFollowingUser(user.id)
+
+
     return (
       <div className="flex flex-col gap-y-4 h-screen">
         <nav className="p-1 items-center justify-between gap-y-8">
-          <h1>{params.username}&apos;s Page</h1>
+          <p>Userame: {user.username}</p>
+          <p>user ID: {user.id}</p>
+          <p>is following: {`${isFollowing}`}</p>
         </nav>
       </div>
     )
