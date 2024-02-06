3436ec8cf61193468f108d888b4c6ecc20b27191
Cristian Vargas <cristian.v0223@gmail.com>
2024-02-06 12:51:46 -0600

    created new directory under /app called /[username], and created a page.tsx file which would be the user page. This way when you click the users on sidebar it will take you to that page /username

---
+++ app/(browse)/[username]/page.tsx
@@ -0,0 +1,11 @@
+const UserPage = () => {
+    return (
+        <div className="flex flex-col gap-y-4 h-screen">
+      <nav className="p-1 items-center justify-between gap-y-8">
+        <h1 >User Page</h1>
+      </nav>
+    </div>
+    )
+}
+
+export default UserPage;
