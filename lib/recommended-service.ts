bd1ad2407fe228d5b5bf71bef3e44cecbb65bcd4
Vargas <cristian.a.vargas@accenture.com>
2024-02-06 11:49:42 -0600

    in the recommended-service.ts file, added a setTimeout so there is a 3 second pause loading users, thus rendering the skeletons we created previously in recommended.tsx, user-item.tsx, user-avatar.tsx

--- lib/recommended-service.ts
+++ lib/recommended-service.ts
@@ -2,6 +2,9 @@ import { db } from "./db"
 import { getSelf } from "./auth-service"
 
 export const getRecommended = async () => {
+    //Adds a 3 second pause before loading users which enables us to see the Skeletons we created in recommended.
+    await new Promise(resolve => setTimeout(resolve, 3000));
+
     const users = await db.user.findMany({
         orderBy: {
             createdAt: "desc"
