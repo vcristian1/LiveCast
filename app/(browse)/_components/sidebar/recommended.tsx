62108c5ad68775fcdf7837952d8464884665d3d6
Vargas <cristian.a.vargas@accenture.com>
2024-02-06 11:31:58 -0600

    created UserItemSkeleton in user-item.tsx for the skeleton of the user

--- app/(browse)/_components/sidebar/recommended.tsx
+++ app/(browse)/_components/sidebar/recommended.tsx
@@ -32,7 +32,7 @@ export const Recommended = ({
                     key={user.id} 
                     username={user.username}
                     imageUrl={user.imageUrl}
-                    isLive={true}
+                    isLive={false}
                     />
                 ))}
             </ul>
--- app/(browse)/_components/sidebar/user-item.tsx
+++ app/(browse)/_components/sidebar/user-item.tsx
@@ -58,4 +58,17 @@ export const UserItem = ({
         </Button>
     
     )
+};
+
+export const UserItemSkeleton = ({
+
+}) => {
+    return (
+        <li className="flex items-center gap-x-4 px-3 py-2">
+            <Skeleton className="min-h-[32px] min-w-[32px] rounded-full"/>
+            <div className="flex-1">
+
+            </div>
+        </li>
+    )
 }
