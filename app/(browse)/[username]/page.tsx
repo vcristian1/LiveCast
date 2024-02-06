567ae3ea31b6bc3d13838b842ed9519cfe70ff17
Vargas <cristian.a.vargas@accenture.com>
2024-02-06 14:04:11 -0600

    added UserPageProps so the UserPage can access the username

--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -1,10 +1,18 @@
-const UserPage = () => {
+interface UserPageProps {
+  params: {
+    username: string;
+  }
+}
+
+const UserPage = ({
+  params,
+}: UserPageProps) => {
     return (
-        <div className="flex flex-col gap-y-4 h-screen">
-      <nav className="p-1 items-center justify-between gap-y-8">
-        <h1 >User Page</h1>
-      </nav>
-    </div>
+      <div className="flex flex-col gap-y-4 h-screen">
+        <nav className="p-1 items-center justify-between gap-y-8">
+          <h1>{params.username}&apos;s Page</h1>
+        </nav>
+      </div>
     )
 }
 
