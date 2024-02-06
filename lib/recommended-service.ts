900bd99feb6ea93335cfeb1bc0fd6ebecd72f70a
Vargas <cristian.a.vargas@accenture.com>
2024-02-06 12:26:09 -0600

    remediated recommended-service.ts so that functionally it no longer is retrieving the users from the database including the user, now it will fetch the users in the database that do not have the userId

--- lib/recommended-service.ts
+++ lib/recommended-service.ts
@@ -4,11 +4,36 @@ import { getSelf } from "./auth-service"
 export const getRecommended = async () => {
     //Adds a 3 second pause before loading users which enables us to see the Skeletons we created in recommended.
     // await new Promise(resolve => setTimeout(resolve, 500));
-    const users = await db.user.findMany({
-        orderBy: {
-            createdAt: "desc"
-        },
-    });
+    let userId;
+    try {
+        const self = await getSelf();
+        userId = self.id;
+    } catch (error) {
+        userId = null;
+    }
+
+    let users = []
+
+    if (userId) {
+        users = await db.user.findMany({
+            where: {
+                NOT: {
+                    id: userId
+                }
+            },
+            orderBy: {
+                createdAt: 'desc'
+            }
+        })
+    } else {
+      users = await db.user.findMany({
+          orderBy: {
+              createdAt: "desc"
+          },
+      });
+    }
+    
+    
 
     return users;
 }
