a5517139d7c88b422bd2da50e38c418d48b211d7
Vargas <cristian.a.vargas@accenture.com>
2024-02-07 14:05:54 -0600

    added onUnblock function in block.ts to handle user unblocking and cache invalidation.

--- actions/block.ts
+++ actions/block.ts
@@ -1,4 +1,4 @@
-import { blockUser } from "@/lib/block-service"; // Import the blockUser function from the block-service module
+import { blockUser, unblockUser } from "@/lib/block-service"; // Import the blockUser function from the block-service module
 import { revalidatePath } from "next/cache"; // Import the revalidatePath function from the next/cache module
 
 // Define a function to handle the blocking of a user
@@ -17,4 +17,22 @@ export const onBlock = async (id: string) => {
 
     // Return the blockedUser object
     return blockedUser;
+}
+
+// Define a function to handle the unblocking of a user
+export const onUnblock = async (id: string) => {
+    // unblock the user identified by the provided id
+    const unblockedUser = await unblockUser(id);
+
+    // Invalidate and revalidate the cache for the root path ("/")
+    revalidatePath("/");
+
+    // If the unblocking operation was successful and a unblockedUser object is returned
+    if (unblockedUser) {
+        // Invalidate and revalidate the cache for the unblocked user's profile page
+        revalidatePath(`/${unblockedUser.blocked.username}`);
+    }
+
+    // Return the unblockedUser object
+    return unblockedUser;
 }
