4f82b08691182e772677e7282d7f42de5a909394
Vargas <cristian.a.vargas@accenture.com>
2024-02-07 13:37:35 -0600

    within block-service.ts created blockUser function for blocking users and enforcing block rules

--- lib/block-service.ts
+++ lib/block-service.ts
@@ -41,3 +41,46 @@ export const isBlockedByUser = async (id: string) => {
         return false;
     }
 }
+
+export const blockUser = async (id:string) => {
+    const self = await getSelf();
+
+    if(self.id === id) {
+        throw new Error("Cannot Block Yourself");
+    }
+
+    const otherUser = await db.user.findUnique({
+        where: {
+            id,
+        }
+    });
+
+    if (!otherUser) {
+        throw new Error("User Not Found!")
+    }
+
+    const existingBlock = await db.block.findUnique({
+        where: {
+            blockerId_blockedId: {
+                blockerId: self.id,
+                blockedId: otherUser.id,
+            },
+        },
+    });
+
+    if(existingBlock){
+        throw new Error("This User is already blocked!")
+    }
+
+    const block = await db.block.create({
+        data: {
+            blockerId: self.id,
+            blockedId: otherUser.id
+        },
+        include: {
+            blocked: true,  
+        },
+    });
+
+    return block
+}
