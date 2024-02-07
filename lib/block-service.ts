662beb3388e608564ef0fac6584435203c026045
Vargas <cristian.a.vargas@accenture.com>
2024-02-07 13:25:41 -0600

    added comments to block-service.ts file

--- lib/block-service.ts
+++ lib/block-service.ts
@@ -1,25 +1,31 @@
-import { getSelf } from "./auth-service";
-import { db } from "./db";
+import { getSelf } from "./auth-service"; // Import the getSelf function from the auth-service module
+import { db } from "./db"; // Import the db object from the db module
 
+// Define a function to check if the current user is blocked by another user
 export const isBlockedByUser = async (id: string) => {
     try {
+        // Retrieve information about the current user
         const self = await getSelf();
 
+        // Retrieve information about the other user based on the provided id
         const otherUser = await db.user.findUnique({
             where: {
                 id
             }
-        })
+        });
 
+        // Throw an error if the other user is not found
         if (!otherUser) {
             throw new Error("User not found");
         }
 
-        // If the otherUser.id is equal to our id return false
-        if(otherUser.id === self.id){
+        // Check if the other user's id is equal to the current user's id
+        // If true, return false (current user cannot block themselves)
+        if (otherUser.id === self.id){
             return false;
         }
 
+        // Check if there exists a block record where the other user is the blocker and the current user is blocked
         const existingBlock = await db.block.findFirst({
             where: {
                 blockerId: otherUser.id,
@@ -27,9 +33,10 @@ export const isBlockedByUser = async (id: string) => {
             },
         });
 
-        //Return a boolean on whether or not existingBlock exists.
-        return !!existingBlock
+        // Return a boolean indicating whether or not an existing block record was found
+        return !!existingBlock;
     } catch (error) {
+        // If an error occurs during the execution, return false
         return false;
     }
 }
