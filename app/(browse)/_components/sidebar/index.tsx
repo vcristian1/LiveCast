9818fd12056a3321f208a712f3ae71b845ad1ff0
Vargas <cristian.a.vargas@accenture.com>
2024-02-07 11:55:57 -0600

    added follows variable which is equal to function getFollowedUsers in index.tsx of the /Sidebar directory as we are getting ready to add the Following section to the sidebar which will show the followed users

--- app/(browse)/_components/sidebar/index.tsx
+++ app/(browse)/_components/sidebar/index.tsx
@@ -2,9 +2,14 @@ import { getRecommended } from "@/lib/recommended-service"
 import { Recommended, RecommendedSkeleton } from "./recommended"
 import { Toggle, ToggleSkeleton } from "./toggle"
 import { Wrapper } from "./wrapper"
+import { getFollowedUsers } from "@/lib/follow-service"
 
 export const Sidebar = async () => {
+    //recommended is equal to a function used to get the recommended users under 'For you'
     const recommended = await getRecommended();
+    //follows is equal to a function used to get the followed users under 'Following'
+    const follows = await getFollowedUsers();
+    
     return (
         <Wrapper>
            <Toggle />
