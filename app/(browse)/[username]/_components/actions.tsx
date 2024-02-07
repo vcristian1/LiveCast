e4402a8a87d1e97e4df36ef608781cf52381c044
Vargas <cristian.a.vargas@accenture.com>
2024-02-07 11:35:59 -0600

    the onClick function in actions.tsx was only handling onFollow(). Updated the name of the original onClick to handleFollow, then made a duplicate of the function and named it handleUnfollow which instead of calling onFollow calls onUnfollow. Added new onClick function which contains an if statement where if isFollowing is true to call handleUnfollow, and if anything else call handleFollow. This way the correct functionality is done depending on which button is rendering

--- app/(browse)/[username]/_components/actions.tsx
+++ app/(browse)/[username]/_components/actions.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { onFollow } from "@/actions/follow";
+import { onFollow, onUnfollow } from "@/actions/follow";
 import { Button } from "@/components/ui/button";
 import { useTransition } from "react";
 import { toast } from "sonner";
@@ -17,13 +17,30 @@ export const Actions = ({
     // Used for loading state
     const [isPending, startTransition] = useTransition();
 
-    const onClick = () => {
+    const handleFollow = () => {
         startTransition(() => {
             onFollow(userId)
                 .then((data) => toast.success(`You are now following ${data.following.username}`))
                 .catch(() => toast.error("Something went wrong!"));
         });  
     };
+
+    const handleUnfollow = () => {
+        startTransition(() => {
+            onUnfollow(userId)
+                .then((data) => toast.success(`You have unfollowed ${data.following.username}`))
+                .catch(() => toast.error("Something went wrong!"));
+        });  
+    };
+
+    const onClick = () => {
+        if(isFollowing) {
+            handleUnfollow();
+        } else {
+            handleFollow();
+        }
+    }
+
     return (
        <Button disabled={isPending} onClick={onClick} variant="primary">
         {isFollowing ? "Unfollow" : "Follow"}
