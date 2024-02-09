a5162ebb00f526584087238df5f7410085daa8f8
Vargas <cristian.a.vargas@accenture.com>
2024-02-09 13:49:46 -0600

    created NavItemSkeleton in nav-item.tsx

--- app/(dashboard)/u/[username]/_components/sidebar/nav-item.tsx
+++ app/(dashboard)/u/[username]/_components/sidebar/nav-item.tsx
@@ -50,7 +50,10 @@ export const NavItem = ({
 export const NavItemSkeleton = () => {
     return (
         <li className="flex items-center gap-x-4 px-3 py-2">
-            <Skeleton />
+            <Skeleton className="min-h-[48px] min-w-[48px] rounded-md" />
+            <div className="flex-1 hidden lg:block">
+                <Skeleton className="h-6" />
+            </div>
         </li>
     )
 }
