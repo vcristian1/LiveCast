3d553532baec65b4fa11ce3add6794323e76dc94
Vargas <cristian.a.vargas@accenture.com>
2024-02-09 13:32:47 -0600

    in navigation.tsx created an array of objects which contain the route urls for stream, keys, chat, and community: /u//

--- app/(dashboard)/u/[username]/_components/sidebar/navigation.tsx
+++ app/(dashboard)/u/[username]/_components/sidebar/navigation.tsx
@@ -1,9 +1,43 @@
 "use client";
 
+import { useUser } from "@clerk/nextjs";
+import { usePathname } from "next/navigation";
+import { Fullscreen, KeyRound, MessageSquare, Users } from "lucide-react";
+
 export const Navigation = () => {
+    const pathname = usePathname();
+    const { user } = useUser();
+
+    const routes = [
+        {
+            label: "Stream",
+            href: `/u/${user?.username}`,
+            icon: Fullscreen,
+        },
+        {
+            label: "Keys",
+            href: `/u/${user?.username}/keys`,
+            icon: KeyRound,
+        },
+        {
+            label: "Chat",
+            href: `/u/${user?.username}/chat`,
+            icon: MessageSquare,
+        },
+        {
+            label: "Community",
+            href: `/u/${user?.username}/community`,
+            icon: Users,
+        },
+    ]
+
     return  (
-        <div>
-            
-        </div>
+        <ul>
+            {routes.map((route) => (
+                <div key={route.href}>
+                    {route.label}
+                </div>
+            ))}
+        </ul>
     )
 }
