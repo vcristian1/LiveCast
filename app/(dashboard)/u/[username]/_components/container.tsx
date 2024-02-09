3c42f3a0f4c04ef1c7aaac9b299058d876e2a8a1
Vargas <cristian.a.vargas@accenture.com>
2024-02-09 13:06:56 -0600

    added comments to Container.tsx and implemented logic to automatically collapse the sidebar on screens with a width less than or equal to 1024px, and that the sidebar is expanded for larger screens.

--- app/(dashboard)/u/[username]/_components/container.tsx
+++ app/(dashboard)/u/[username]/_components/container.tsx
@@ -1,9 +1,7 @@
-"use client";
-
 import { useEffect } from "react";
-import { useMediaQuery } from "usehooks-ts";
+import { useMediaQuery } from "usehooks-ts"; // Importing useMediaQuery hook
 import { cn } from "@/lib/utils";
-import { useCreatorSidebar } from "@/store/use-creator-sidebar";
+import { useCreatorSidebar } from "@/store/use-creator-sidebar"; // Importing custom hook for creator sidebar functionality
 
 interface ContainerProps {
     children: React.ReactNode;
@@ -12,9 +10,32 @@ interface ContainerProps {
 export const Container = ({
     children,
 }: ContainerProps) => {
+    // Destructuring collapsed, onExpand, and onCollapse from useCreatorSidebar hook
+    const { collapsed, onExpand, onCollapse } = useCreatorSidebar((state) => state);
+
+    // Using useMediaQuery to check screen size
+    const matches = useMediaQuery(`(max-width: 1024px)`);
+
+    useEffect(() => {
+        // Effect to handle sidebar behavior based on screen size
+        if (matches) {
+            // If screen width is less than or equal to 1024px, collapse sidebar
+            onCollapse();
+        } else {
+            // If screen width is greater than 1024px, expand sidebar
+            onExpand();
+        }
+    }, [matches, onCollapse, onExpand]); // Dependencies: matches, onCollapse, onExpand
+
     return (
-        <div>
+        // Render children with dynamic margin left based on collapsed state and screen size
+        <div className={cn(
+            "flex-1",
+            // Set margin left to 70px on mobile screens when sidebar is collapsed
+            // Set margin left to 70px on mobile screens and 240px on large screens when sidebar is not collapsed
+            collapsed ? 'ml-[70px]' : 'ml-[70px] lg:ml-60'
+        )}>
             {children}
         </div>
-    )
-}
+    );
+};
