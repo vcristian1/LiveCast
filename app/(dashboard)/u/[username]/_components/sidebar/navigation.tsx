0749b48b7a3f26add16b22883f699feb2d9b84fb
Cristian Vargas <cristian.v0223@gmail.com>
2024-02-11 00:47:15 -0600

    added conditional logic to wrap the navitem with hint if collapsed is true, so that the sidebar links for the creator page have hints when collpased but the hints do not show when it is not collapsed

--- app/(dashboard)/u/[username]/_components/sidebar/navigation.tsx
+++ app/(dashboard)/u/[username]/_components/sidebar/navigation.tsx
@@ -4,10 +4,13 @@ import { useUser } from "@clerk/nextjs";
 import { usePathname } from "next/navigation";
 import { Fullscreen, KeyRound, MessageSquare, Users } from "lucide-react";
 import { NavItem, NavItemSkeleton } from "./nav-item";
+import { useCreatorSidebar } from "@/store/use-creator-sidebar";
+import { Hint } from "@/components/hint";
 
 export const Navigation = () => {
     const pathname = usePathname();
     const { user } = useUser();
+    const { collapsed } = useCreatorSidebar((state) => state);
 
     const routes = [
         {
@@ -44,15 +47,25 @@ export const Navigation = () => {
 
     return  (
         <ul className="space-y-2 px-2 pt-4 lg:pt-0">
-            {routes.map((route) => (
-                <NavItem 
-                 key={route.href}
-                 label={route.label}
-                 icon={route.icon}
-                 href={route.href}
-                 isActive={pathname === route.href}
-                />
-            ))}
+            {routes.map((route) => {
+                const navItem = (
+                    <NavItem 
+                     label={route.label}
+                     icon={route.icon}
+                     href={route.href}
+                     isActive={pathname === route.href}
+                    />
+                );
+
+                // Conditionally wrap NavItem with Hint based on the collapsed state
+                return collapsed ? (
+                    <Hint label={route.label} side="right" key={route.href}>
+                        {navItem}
+                    </Hint>
+                ) : (
+                    navItem
+                );
+            })}
         </ul>
-    )
-}
+    );
+};
