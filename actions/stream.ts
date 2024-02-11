5de67e9b02ff4df58971866058b740d68889ecc3
Cristian Vargas <cristian.v0223@gmail.com>
2024-02-11 01:49:54 -0600

    remedited stream.ts so the switch would actually change the boolean in the db associated with isChatEnabled

--- actions/stream.ts
+++ actions/stream.ts
@@ -1,45 +1,47 @@
-"use service"
+"use server";
 
-import { db } from "@/lib/db"
-import { revalidatePath } from "next/cache"
-import { Stream } from "@prisma/client"
-import { getSelf } from "@/lib/auth-service"
+import { Stream } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+
+import { db } from "@/lib/db";
+import { getSelf } from "@/lib/auth-service";
 
 export const updateStream = async (values: Partial<Stream>) => {
-    try {
-        const self = await getSelf();
-        const selfStream = await db.stream.findUnique({
-            where: {
-                userId: self.id,
-            }
-        })
+  try {
+    const self = await getSelf();
+    const selfStream = await db.stream.findUnique({
+      where: {
+        userId: self.id,
+      },
+    });
 
-        if (!self) {
-            throw new Error("Stream not found")
-        }
-
-        const validData = {
-            name: values.name,
-            isChatEnabled: values.isChatEnabled,
-            isChatFollowersonly: values.isChatFollowersOnly,
-            isChatDelayed: values.isChatDelayed,
-        }
-
-        const stream = await db.stream.update({
-            where: {
-                id: selfStream?.id,
-            },
-            data: {
-                ...validData,
-            },
-        });
-
-        revalidatePath(`/u/${self.username}/chat`)
-        revalidatePath(`/u/${self.username}`)
-        revalidatePath(`/${self.username}`)
-
-        return stream;
-    } catch (error) {
-        throw new Error("Internal Error")
+    if (!selfStream) {
+      throw new Error("Stream not found");
     }
-}
+
+    const validData = {
+      thumbnailUrl: values.thumbnailUrl,
+      name: values.name,
+      isChatEnabled: values.isChatEnabled,
+      isChatFollowersOnly: values.isChatFollowersOnly,
+      isChatDelayed: values.isChatDelayed,
+    };
+
+    const stream = await db.stream.update({
+      where: {
+        id: selfStream.id,
+      },
+      data: {
+        ...validData,
+      },
+    });
+
+    revalidatePath(`/u/${self.username}/chat`);
+    revalidatePath(`/u/${self.username}`);
+    revalidatePath(`/${self.username}`);
+
+    return stream;
+  } catch {
+    throw new Error("Internal Error");
+  };
+};
