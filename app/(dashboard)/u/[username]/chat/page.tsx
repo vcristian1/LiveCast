1129a7dc91491d2412da916c280c9d9b5e615804
Cristian Vargas <cristian.v0223@gmail.com>
2024-02-11 01:31:18 -0600

    in page.tsx created div containing new component Switch from shadcn which ideally will eanble to the user to enable chat for their stream with a switch

--- app/(dashboard)/u/[username]/chat/page.tsx
+++ app/(dashboard)/u/[username]/chat/page.tsx
@@ -1,5 +1,6 @@
 import { getSelf } from "@/lib/auth-service";
 import { getStreamByUserId } from "@/lib/stream-service";
+import { ToggleCard } from "./_components/toggle-card";
 
 const ChatPage = async () => {
     const self = await getSelf();
@@ -8,13 +9,17 @@ const ChatPage = async () => {
     if(!stream) {
         throw new Error("Stream not found");
     }
-    
+
     return (
         <div className="p-6">
             <div className="mb-4">
                 <h1 className="text-2xl font-bold">
                     Chat Settings
                 </h1>
+            </div>
+
+            <div className="space-y-2">
+                <ToggleCard field="isChatEnabled" label="Enable chat" value={stream.isChatEnabled}/>
             </div>
         </div>
     )
