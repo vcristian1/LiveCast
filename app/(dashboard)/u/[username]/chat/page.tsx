698ef3cbd43bced0c3e35d94e6dd64a435759cdc
Cristian Vargas <cristian.v0223@gmail.com>
2024-02-11 01:09:39 -0600

    added basic styling to chat page

--- app/(dashboard)/u/[username]/chat/page.tsx
+++ app/(dashboard)/u/[username]/chat/page.tsx
@@ -1,7 +1,11 @@
 const ChatPage = () => {
     return (
-        <div>
-            Chat Page
+        <div className="p-6">
+            <div className="mb-4">
+                <h1 className="text-2xl font-bold">
+                    Chat Settings
+                </h1>
+            </div>
         </div>
     )
 }
