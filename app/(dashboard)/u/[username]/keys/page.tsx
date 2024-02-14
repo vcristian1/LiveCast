50e1687530a6c64e70c8a2c4bd7211355f280c39
Cristian Vargas <cristian.v0223@gmail.com>
2024-02-14 15:40:05 -0600

    added Keys & URLs heading, added Input compononent from ShadCN UI, and added Button from ShadCN UI for Generating the Server URL

--- app/(dashboard)/u/[username]/keys/page.tsx
+++ app/(dashboard)/u/[username]/keys/page.tsx
@@ -1,7 +1,25 @@
-export const KeysPage = () => {
+import { Button } from "@/components/ui/button";
+import { UrlCard } from "./_components/url-card";
+import { getStreamByUserId } from "@/lib/stream-service";
+import { getSelf } from "@/lib/auth-service";
+
+export const KeysPage = async () => {
+    const self = await getSelf();
+    const stream = await getStreamByUserId(self.id)
+
+    if(!stream) {
+        throw new Error("Stream not found")
+    }
+
     return (
-        <div>
-            Keys page
+        <div className="p-6">
+            <div className="flex items-center justify-between mb-4">
+                <h1 className="text-2xl font-bold">Keys & URLs</h1>
+                <Button size="sm" variant="primary">Generate</Button>
+            </div>
+            <div className="space-y-4">
+                <UrlCard value={stream.serverUrl} />
+            </div>
         </div>
     )
 }
