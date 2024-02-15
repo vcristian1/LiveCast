056ce7a98c9bf4f4c4fb27f37cc7f6312f3b29f9
Cristian Vargas <cristian.v0223@gmail.com>
2024-02-15 16:24:30 -0600

    created key-card.tsx file and imported in page.tsx uner the Server URL portion of the page

--- app/(dashboard)/u/[username]/keys/_components/key-card.tsx
+++ app/(dashboard)/u/[username]/keys/_components/key-card.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Input } from "@/components/ui/input";
+
 interface KeyCardProps {
     value: string | null;
 };
@@ -8,8 +10,17 @@ export const KeyCard = ({
     value
 }: KeyCardProps) => {
     return (
-        <div>
-
+        <div className="rounded-xl bg-muted p-6">
+            <div className="flex items-start gap-x-10">
+                <p className="font-semibold shrink-0">
+                    Stream Key
+                </p>
+                <div className="space-y-2 w-full">
+                    <div className="w-full flex items-center gap-x-2">
+                        <Input value={value || ""} type="password" disabled placeholder="Stream key"/>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
