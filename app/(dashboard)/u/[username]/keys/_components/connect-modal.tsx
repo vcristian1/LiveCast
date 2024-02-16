5a0f0557e7efbfdec485b580172e6cb41244a749
Vargas <cristian.a.vargas@accenture.com>
2024-02-16 12:18:35 -0600

    removed button stating generate in page.tsx and replaced with connect modal component created in connect-modal.tsx which opens a modal using Dialog from ShadCN once you click generate connection on keys page of creator dashboard

--- app/(dashboard)/u/[username]/keys/_components/connect-modal.tsx
+++ app/(dashboard)/u/[username]/keys/_components/connect-modal.tsx
@@ -1,7 +1,28 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { 
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger
+} from "@/components/ui/dialog";
+
 export const ConnectModal = () => {
     return (
-        <div>
-            Connect Modal
-        </div>
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button variant='primary'>
+                    Generate connection
+                </Button>
+            </DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Generate connection</DialogTitle>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
     )
 }
