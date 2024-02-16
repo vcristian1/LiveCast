0e2b11f14f026b5f08efc8e6b67b64913311cb0b
Vargas <cristian.a.vargas@accenture.com>
2024-02-16 12:54:34 -0600

    added Alert within the Modal warning the user the action will reset all active streams. Included Alert Triangle from lucide react, dialog close button for cancelling, and another button to generate the connection

--- app/(dashboard)/u/[username]/keys/_components/connect-modal.tsx
+++ app/(dashboard)/u/[username]/keys/_components/connect-modal.tsx
@@ -9,6 +9,12 @@ import {
     DialogTitle,
     DialogTrigger
 } from "@/components/ui/dialog";
+import {
+    Alert,
+    AlertDescription,
+    AlertTitle,
+} from "@/components/ui/alert"
+import { AlertTriangle } from "lucide-react";
 
 export const ConnectModal = () => {
     return (
@@ -22,6 +28,23 @@ export const ConnectModal = () => {
                 <DialogHeader>
                     <DialogTitle>Generate connection</DialogTitle>
                 </DialogHeader>
+                <Alert>
+                    <AlertTriangle className="h-4 w-4"/>
+                    <AlertTitle>Warning!</AlertTitle>
+                    <AlertDescription>
+                        This action will reset all active streams using the current connection
+                    </AlertDescription>
+                </Alert>
+                <div className="flex justify-between">
+                    <DialogClose>
+                        <Button variant={`ghost`}>
+                            Cancel
+                        </Button>
+                    </DialogClose>
+                    <Button variant="primary" onClick={() => {}}>
+                        Generate
+                    </Button>
+                </div>
             </DialogContent>
         </Dialog>
     )
