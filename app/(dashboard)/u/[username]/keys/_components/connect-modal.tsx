3ef80cc3694d288dc69533c396ae263411b213ad
Vargas <cristian.a.vargas@accenture.com>
2024-02-16 13:16:28 -0600

    imported Select from shadcnui and added Select with SelectTrigger,SelectValue,SelectContent, and SelectItem so the user is able to select the connection type within the modal once generate connection is clicked in keys page of creator dashboard

--- app/(dashboard)/u/[username]/keys/_components/connect-modal.tsx
+++ app/(dashboard)/u/[username]/keys/_components/connect-modal.tsx
@@ -15,6 +15,13 @@ import {
     AlertTitle,
 } from "@/components/ui/alert"
 import { AlertTriangle } from "lucide-react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from "@/components/ui/select"
 
 export const ConnectModal = () => {
     return (
@@ -28,6 +35,15 @@ export const ConnectModal = () => {
                 <DialogHeader>
                     <DialogTitle>Generate connection</DialogTitle>
                 </DialogHeader>
+                <Select>
+                    <SelectTrigger>
+                        <SelectValue placeholder="Ingress Type"/>
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectItem value="RTMP">RTMP</SelectItem>
+                        <SelectItem value="WHIP">WHIP</SelectItem>
+                    </SelectContent>
+                </Select>
                 <Alert>
                     <AlertTriangle className="h-4 w-4"/>
                     <AlertTitle>Warning!</AlertTitle>
